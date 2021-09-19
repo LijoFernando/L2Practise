c07b1820e746e92401142fafca7e8f6be5807699
LijoFernando <rotricklijo@gmail.ocm>
2021-09-19 14:26:59 +0530

    sort task

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -4,40 +4,62 @@ import java.util.Arrays;
 
 public class MergeSort {
     //merger
-    public static void sort(int[] arr, int left, int mid, int right){
-        int n1 = mid+1;
+    public static void sort(int[] arr, int left, int mid, int right) {
+        int n1 = mid-left+1;
         int n2 = right-mid;
-        int [] aryLeft = new int[n1];
-        int [] aryRight = new int[n2];
-        int i=0;
-        int j=0;
-       while(left<=mid){
-           aryLeft[i]=arr[left];
-           left++;
-       }
-        while(mid+1<right){
-            aryRight[j]=arr[mid+1];
-            mid++;
-        }
-        System.out.println(Arrays.toString(aryLeft));
-        System.out.println(Arrays.toString(aryRight));
-    }
-    //sort
-    public static void mergeSort(int arr[], int start , int end){
-        if(start<end) {
-            int mid = (start+end) / 2;
-            System.out.print(arr[mid]+" ");
-            sort(arr,start, mid, end);
-            mergeSort(arr, start, mid);
-            mergeSort(arr, mid + 1, end);
+        int[] aryLeft = new int[n1];
+        int[] aryRight = new int[n2];
+        for (int i = 0; i < n1; i++) aryLeft[i]= arr[left+i];
+        for (int j = 0; j < n2; j++) aryRight[j]= arr[mid+1+j];
+        System.out.print(Arrays.toString(aryLeft)+" ");
+        System.out.print(Arrays.toString(aryRight)+" ");
+        System.out.println();
 
+        //check subaarray from i1....& i2......
+        int i1=0,i2=0;
+        int k =left;
+        while(i1<n1 && i2<n2){
+            if(aryLeft[i1]>=aryRight[i2]) {
+                arr[k]= aryRight[i2];
+                i2++;
+            }
+            else{
+                arr[k]=aryLeft[i1];
+                i1++;
+            }
+            k++;
+        }
+        //Remaining ELements push into Main arry
+        while(i1<n1){
+            arr[k]=aryLeft[i1];
+            i1++;
+             k++;
+        }
+        while(i2<n2){
+            arr[k]=aryRight[i2];
+            i2++;
+            k++;
+        }
+
+    }
+
+    //sort
+    public static void mergeSort(int arr[], int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+         //   System.out.print(arr[mid] + " ");
+          mergeSort(arr, start, mid);
+           mergeSort(arr, mid+1, end);
+            sort(arr, start, mid, end);
         }
     }
+
     //main
     public static void main(String[] args) {
-        int[] arr={ 6, 5, 12, 10, 9, 1 };
-        int start =0;
-        int end =arr.length;
-        mergeSort(arr,start, end);
+        int[] arr = {6, 5, 12, 10, 9, 1};
+        int start = 0;
+        int end = arr.length-1;
+        mergeSort(arr, start, end);
+        System.out.println(Arrays.toString(arr));
     }
 }
