785158eab31189628f04641fdd6359443a003a4c
LijoFernando <rotricklijo@gmail.ocm>
2021-10-05 13:17:10 +0530

    file handling

--- src/sorting/ValidIp.java
+++ src/sorting/ValidIp.java
@@ -1,26 +1,28 @@
 package sorting;
 
 public class ValidIp {
+
     public static boolean isValid(String ip) {
-
+        // Write your code here
         String[] digits = ip.split("\\.");
-        if (digits.length > 4)
-            //  System.out.println("am here");
-            return false;
-
+        if (digits.length == 0) return false;
+        if (digits.length != 4) return false; //  System.out.println("am here");
         for (String str : digits) {
-            //   if(str.contains('a'))
-            long port = Long.parseLong(str);
-            if (!(port >= 0 && port <= 255))
-                //  System.out.println("am here");
+            long port;
+            try {
+                port = Long.parseLong(str);
+                if (!(port >= 0 && port <= 255))
+                    return false;  //  System.out.println("am here"); System.out.println(port);
+            } catch (Exception e) {
                 return false;
-
+            }
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String ip = "74.3.87.78";
+        String ip = "0000.0000.0000.0000";
         System.out.println(isValid(ip));
     }
+
 }
